54abf920d0fe3656cf769ceafedecd9cb7d3ee82
「 LovelyPrince™ 」 <DarlingPrince@protonmail.com>
2022-09-25 11:43:13 +0530

    Update run_code.py

--- NekoUserBot/plugins/run_code.py
+++ NekoUserBot/plugins/run_code.py
@@ -1,3 +1,35 @@
+"""
+BSD 3-Clause License
+
+Copyright (c) 2022, Awesome-Prince
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+1. Redistributions of source code must retain the above copyright notice, this
+   list of conditions and the following disclaimer.
+
+2. Redistributions in binary form must reproduce the above copyright notice,
+   this list of conditions and the following disclaimer in the documentation
+   and/or other materials provided with the distribution.
+
+3. Neither the name of the copyright holder nor the names of its
+   contributors may be used to endorse or promote products derived from
+   this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+"""
+
 import io
 import sys
 import traceback
@@ -12,7 +44,7 @@ from NekoUserBot import neko
 @neko.on_message(filters.user(OWNER_ID) & filters.command("logs", prefixes=HANDLER))
 def logs(_, m):
     run_logs = run("tail logs.txt")
-    msg = m.reply_text("sᴇɴᴅɪɴɢ ʟᴏɢs...")
+    msg = m.reply_text("sending logs...")
     with io.BytesIO(str.encode(run_logs)) as logs:
         logs.name = "neko.txt"
         m.reply_document(
