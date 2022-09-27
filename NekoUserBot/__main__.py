f1da2c24078f8085f7196b8df0e4e46402729e54
« ͸ Tʜᴇ ɢᴛᴀsʜ ۩ <114175128+Thegtash@users.noreply.github.com>
2022-09-27 11:33:26 +0530

    Update __main__.py

--- NekoUserBot/__main__.py
+++ NekoUserBot/__main__.py
@@ -1,7 +1,7 @@
 """
 BSD 3-Clause License
 
-Copyright (c) 2022, Awesome-Prince
+Copyright (c) 2022, Thegtash
 All rights reserved.
 
 Redistribution and use in source and binary forms, with or without
